14477987f790bc93b1dc0aab7403acdd0b203aec
Malte Breuer <malte.breuer@gmail.com>
2019-11-19 12:39:13 +0100

    Initial commit

---
+++ get.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+import requests
+from requests.auth import HTTPBasicAuth
+
+# Load the environment variables from .env
+load_dotenv()
+
+# Import URLs relative to the base URL
+JIRA_VELOCITY_URL = os.environ['JIRA_BASE_URL'] + "/rest/greenhopper/latest/rapid/charts/velocity?rapidViewId=" + os.environ["RAPID_BOARD_ID"]
+
+print("""
+     ██╗██╗██████╗  █████╗     ██╗  ██╗██████╗ ██╗
+     ██║██║██╔══██╗██╔══██╗    ██║ ██╔╝██╔══██╗██║
+     ██║██║██████╔╝███████║    █████╔╝ ██████╔╝██║
+██   ██║██║██╔══██╗██╔══██║    ██╔═██╗ ██╔═══╝ ██║
+╚█████╔╝██║██║  ██║██║  ██║    ██║  ██╗██║     ██║
+ ╚════╝ ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝     ╚═╝
+     _                     _                 _           
+    | |                   | |               | |          
+  __| | _____      ___ __ | | ___   __ _  __| | ___ _ __ 
+ / _` |/ _ \ \ /\ / / '_ \| |/ _ \ / _` |/ _` |/ _ \ '__|
+| (_| | (_) \ V  V /| | | | | (_) | (_| | (_| |  __/ |   
+ \__,_|\___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|\___|_|   
+                                                         
+                                            
+                                                                                                                                            
+""")
+print(os.environ['JIRA_BASE_URL'])
+r = requests.get(JIRA_VELOCITY_URL, auth=HTTPBasicAuth(os.environ['JIRA_USER'], os.environ['JIRA_PASSWORD']))
+print(r)
